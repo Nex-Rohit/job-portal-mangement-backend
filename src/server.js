9405ca68b9ecf4e7c69916d8184d8025c2ad055a
Nex-Rohit <rohit0402@nexzem.com>
2026-04-21 16:50:39 +0530

    Add:Add server file

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import app from "./app.js";
+import dotenv from 'dotenv'
+dotenv.config();
+
+const PORT=process.env.PORT||8080
+
+app.listen(PORT,()=>{
+    console.log("=========================\n");
+    console.log(`SERVER IS LISTENING ON http://localhost:${PORT}\n`);
+    console.log("=========================");
+})
