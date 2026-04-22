5d034c1ff347c5dbf58714a90d89feb9ab84f7b9
Nex-Rohit <rohit0402@nexzem.com>
2026-04-22 16:44:00 +0530

    Add:Add jsonwebtoken authentication

--- src/middleware/jwtVerifyMiddleware.js
+++ src/middleware/jwtVerifyMiddleware.js
@@ -1,1 +1,23 @@
-// TODO
+import jwt from 'jsonwebtoken';
+import { sendError } from '../utils/errorHandler.js';
+import sendSuccess from '../utils/responseHandler.js';
+import asyncHandler from '../utils/asyncHandler.js';
+
+
+const jwtVerifyMiddleware = asyncHandler(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return sendError(res, 401, 'Unauthorized: No token provided');
+    }
+
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        return sendError(res, 401, 'Unauthorized: Invalid token');
+    }
+});
+
+export default jwtVerifyMiddleware;
