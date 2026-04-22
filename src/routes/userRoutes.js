ad8a3f0cf95cc027d5c83b5ebcdebc0633b322ac
Nex-Rohit <rohit0402@nexzem.com>
2026-04-22 16:45:19 +0530

    Add:Add sign-up,login,getuserDetails,updateUserDetails,AddEducationDetails,getEducationDetails routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,1 +1,47 @@
-// TODO
+import express from "express";
+import {
+  UserRegister,
+  UserLogin,
+  getUserDetails,
+  updateUserDetails,
+  AddEducationDetails,
+  GetEducationDetails,
+} from "../controller/userController.js";
+import { authorize } from "../middleware/authorize.js";
+import jwtVerifyMiddleware from "../middleware/jwtVerifyMiddleware.js";
+const router = express.Router();
+import upload from "../utils/multer.js";
+
+router.post("/sign-up", UserRegister);
+router.post("/login", UserLogin);
+router.get(
+  "/profile",
+  jwtVerifyMiddleware,
+  authorize(["user"]),
+  
+  getUserDetails,
+);
+router.patch(
+  "/profile",
+  jwtVerifyMiddleware,
+  authorize(["user"]),
+  upload.fields([
+    { name: "profilePhoto", maxCount: 1 },
+    { name: "resume", maxCount: 1 },
+  ]),
+  updateUserDetails,
+);
+router.post(
+  "/education",
+  jwtVerifyMiddleware,
+  authorize(["user"]),
+  AddEducationDetails,
+);
+router.get(
+  "/education",
+  jwtVerifyMiddleware,
+  authorize(["user"]),
+  GetEducationDetails,
+);
+
+export default router;
